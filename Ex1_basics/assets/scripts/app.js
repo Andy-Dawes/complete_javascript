e05abbab8ca03ebb9c4e4ff1a5b4c6eb520b0517
Andy Dawes <andy.dawes@live.co.uk>
2020-06-09 19:57:57 +0100

    Mop up app.js &  vendor.js

--- Ex1_basics/assets/scripts/app.js
+++ Ex1_basics/assets/scripts/app.js
@@ -1,48 +1,47 @@
-// Variables
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
-// Functions
-
+// Gets input from input field
 function getUserNumberInput() {
-  return parseInt(userInput.value);
+  return parseInt(usrInput.value);
 }
 
+// Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-  const calcDescription = "${resultBeforeCalc} ${operator} ${calcNumber}";
-  outputResult(currentResult, calcDescription);
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); // from vendor file
 }
 
 function add() {
-  const enterNumber = getUserNumberInput();
-  const initalResult = currentResult;
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
   currentResult = currentResult + enteredNumber;
-  createAndWriteOutput("+", initialResult, currentResult);
+  createAndWriteOutput("+", initialResult, enteredNumber);
+  logEntries.push(enteredNumber);
+  console.log(logEntries);
 }
 
 function subtract() {
-  const enterNumber = getUserNumberInput();
-  const initalResult = currentResult;
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
   currentResult = currentResult - enteredNumber;
-  createAndWriteOutput("-", initialResult, currentResult);
+  createAndWriteOutput("-", initialResult, enteredNumber);
 }
 
 function multiply() {
-  const enterNumber = getUserNumberInput();
-  const initalResult = currentResult;
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
   currentResult = currentResult * enteredNumber;
-  createAndWriteOutput("*", intialResult, currentResult);
+  createAndWriteOutput("*", initialResult, enteredNumber);
 }
 
 function divide() {
-  const enterNumber = getUserNumberInput();
-  const initalResult = currentResult;
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
   currentResult = currentResult / enteredNumber;
-  createAndWriteOutput("/", initialResult, currentResult);
+  createAndWriteOutput("/", initialResult, enteredNumber);
 }
-
-// currentResult = add(1, 2);
-// let calculationDescription = "(${defaultResult} + 10) * 3 / 2 - 1";
 
 addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtract);
