506388e670444a505e5b9e02c7adefc533a21fdd
Andy Dawes <andy.dawes@live.co.uk>
2020-06-16 21:25:23 +0100

    + hasBonusLife; const initialPlayerHealth; removeBonusLife

--- Ex2_monster-killer/assets/scripts/app.js
+++ Ex2_monster-killer/assets/scripts/app.js
@@ -6,14 +6,22 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound() {
+    const initialPlayerHealth = currentPlayerHealth;
     // const damage = dealMonsterDamage(maxDamage);
     // currentMonsterHealth -= damage;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
+
+    if (currentPlayerHealth <= 0 && hasBonusLife) {
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+    }
 
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert("You won!");
