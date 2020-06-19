a3f8bdb376adad57ed8c720d534b6f78c361df46
Andy Dawes <andy.dawes@live.co.uk>
2020-06-19 14:17:02 +0100

    + func WriteToLog & consts LOG_EVENT_etc

--- Ex2_monster-killer/assets/scripts/app.js
+++ Ex2_monster-killer/assets/scripts/app.js
@@ -3,13 +3,23 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-const MODE_ATTACK = 'ATTACK';
-const MODE_STRONG_ATTACK = 'STRONG_ATTACK';
+const MODE_ATTACK = "ATTACK";
+const MODE_STRONG_ATTACK = "STRONG_ATTACK";
+const LOG_EVENT_PLAYER_ATTACK = "PLAYER_ATTACK";
+const LOG_EVENT_PLAYER_STRONG_ATTACK = "PLAYER_STRONG_ATTACK";
+const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
+const LOG_EVENT_PLAYER_HEAL = "PLAYER_HEAL";
+const LOG_EVENT_GAME_OVER = "GAME_OVER";
 
-const enteredValue = prompt('Maximum life for you and the monster? Enter here: ', '100');
+const enteredValue = prompt(
+    "Maximum life for you and the monster? Enter here: ",
+    "100"
+);
 
 let chosenMaxLife = parseInt(enteredValue);
-if (isNAN(chosenMaxLife) || chosenMaxLife <= 0) {
+let battleLog = [];
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
     chosenMaxLife = 100;
 }
 
@@ -18,6 +28,19 @@ let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
+
+function WriteToLog(ev, monsterHealth) {
+    let logEntry;
+    if (ev === LOG_EVENT_PLAYER_ATTACK) {
+        logEntry = {
+            event: ev,
+            value: val,
+            finalMonsterHealth: monsterHealth,
+            finalPlayerHealth: finalPlayerHealth
+        };
+        battleLog.push(logEntry);
+    }
+}
 
 function reset() {
     currentMonsterHealth = chosenMaxLife;
@@ -53,9 +76,9 @@ function endRound() {
 
 function attackMonster(mode) {
     let maxDamage;
-    if (mode === "ATTACK") {
+    if (mode === MODE_ATTACK) {
         maxDamage = ATTACK_VALUE;
-    } else if (mode === "STRONG_ATTACK") {
+    } else if (mode === MODE_STRONG_ATTACK) {
         maxDamage = STRONG_ATTACK_VALUE;
     }
     const damage = dealMonsterDamage(maxDamage);
@@ -64,11 +87,11 @@ function attackMonster(mode) {
 }
 
 function attackHandler() {
-    attackMonster("ATTACK");
+    attackMonster(MODE_ATTACK);
 }
 
 function strongAttackHandler() {
-    attackMonster("STRONG_ATTACK");
+    attackMonster(MODE_STRONG_ATTACK);
 }
 
 function healPlayerHandler() {
