e5ce2f913fbad09d170b5080c4627c08317a3236
Andy dawes <andy.dawes@live.co.uk>
2020-07-04 10:18:47 +0100

    + func getPlayerChoice

--- Ex3_rock_paper_scissors/app.js
+++ Ex3_rock_paper_scissors/app.js
@@ -1,15 +1,20 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
-// const person = {
-//     greet: function greet() {
-//         console.log("Hello there!");
-//     }
-// };
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
 
-// person.greet();
-
-// console.log(typeof startGame());
-
-startGameBtn.addEventListener("click", function) {
-    console.log("The game is starting!");
+const getPlayerChoice = function () {
+    const selection = prompt('Choose Rock, Paper or Scissors: ', ' ');
+    if (
+        selection !== 'ROCK' &&
+        selection !== 'PAPER' &&
+        selection !== 'SCISSORS'
+    ) {
+        alert('You made an invalid choice. We chose Rock for you');
+    }
 }
+
+startGameBtn.addEventListener("click", function startGame() {
+    console.log("The game is starting!");
+});
